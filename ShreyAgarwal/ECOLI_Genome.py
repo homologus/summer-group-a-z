ec69c82f02a48cfdb4a718f33e96f846f5b7ca23
ShreyAgarwal310 <ShreyAgarwal310@github.com>
2020-06-15 15:11:31 +0000

    ShreyAgarwal

--- ShreyAgarwal/ECOLI_Genome.py
+++ ShreyAgarwal/ECOLI_Genome.py
@@ -1,4 +1,7 @@
-f = open('ECOLI_Genome.txt')
-for x  in f:
-    print(x)
+f = open("ECOLI_Genome.txt", "r")
+string = f.readlines()
+string = [element.rstrip() for element in string]
+DNA = ""
+DNA = DNA.join(string)
+print(DNA)
 f.close()
