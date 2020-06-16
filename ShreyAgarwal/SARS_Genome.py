a9e68ca85a0d9fc0f56c766dc231aeee8559e1c6
ShreyAgarwal310 <ShreyAgarwal310@github.com>
2020-06-16 16:00:15 +0000

    ShreyAgarwal

--- ShreyAgarwal/SARS_Genome.py
+++ ShreyAgarwal/SARS_Genome.py
@@ -1,3 +1,13 @@
+* from Bio.seq import Seq
 f = open("SARS.txt", "r")
+first_line = f.readline()
 string = f.readlines()
+string = [genome.rstrip() for genome in string]
+RNA = ""
+RNA = RNA.join(string)
+read = Seq(RNA)
+
+print(read[10:20])
+
+f.close()
 
