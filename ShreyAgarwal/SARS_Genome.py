62570883f2776147d3ac7f02580e1b66b8bd0aaa
ShreyAgarwal310 <ShreyAgarwal310@github.com>
2020-06-17 15:18:46 +0000

    ShreyAgarwal

--- ShreyAgarwal/SARS_Genome.py
+++ ShreyAgarwal/SARS_Genome.py
@@ -1,15 +1,32 @@
 from Bio.Seq import Seq
+
 f = open("SARS.txt", "r")
 first_line = f.readline()
 string = f.readlines()
-string = [genome.rstrip() for genome in string]
+string = [item.rstrip() for item in string]
 RNA = ""
 RNA = RNA.join(string)
+print(len(RNA))
+x = 0
+Protein_Start = 0
+Stop_Codon = ""
+Protein_Seq = ""
+
 read = Seq(RNA)
 
-print(read.translate())
-print(read[1:].translate())
-print(read[2:].translate())
-
-f.close()
+while x < len(RNA):
+        if RNA[x:x + 3] == "TAA" or RNA[x:x + 3] == "TAG" or RNA[x:x + 3] == "TGA":
+                Stop_Codon = RNA[Protein_Start: x + 1]
+                y = Protein_Start - 3
+                while y < x:
+                        y = y + 3;
+                        Protein_Seq = Protein_Seq.join(read[y:y + 3].translate())
+                if len(Protein_Seq) > 100:
+                        print("Start of Protein:", Protein_Start, "End of Protein:", i, "Protein Sequence:", Protein_Seq)
+                Protein_Start = x + 1
+                x = x + 4
+        else:
+                x = x + 3
+        Protein_Seq = ""
+        DNAUnitilStopCode = ""
 
