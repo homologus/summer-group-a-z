b1c10a17bba92b3e8d6aa599ece7e56934b4a13e
ShreyAgarwal310 <ShreyAgarwal310@github.com>
2020-06-18 16:09:31 +0000

    ShreyAgarwal

--- ShreyAgarwal/SARS_Genome.py
+++ ShreyAgarwal/SARS_Genome.py
@@ -6,6 +6,7 @@ string = f.readlines()
 string = [item.rstrip() for item in string]
 RNA = ""
 RNA = RNA.join(string)
+RNA = RNA[:-2]
 print(len(RNA))
 x = 0
 Protein_Start = 0
@@ -14,16 +15,10 @@ Protein_Seq = ""
 
 read = Seq(RNA)
 
-while x < len(RNA):
-        if RNA[x:x + 3] == "TAA" or RNA[x:x + 3] == "TAG" or RNA[x:x + 3] == "TGA":
-                Stop_Codon = RNA[Protein_Start: x + 3]
-                y = Protein_Start - 3
-                while y < x:
-                        y = y + 3;
-                        Protein_Seq = Protein_Seq.join(read[y:y + 3].translate())
-                if len(Protein_Seq) > 100:
-                        print(Protein_Start, x, Protein_Seq)
-                Protein_Start = x + 1
-                x = x + 4
-        else:
-                x = x + 3
+protein_translate = str(read.translate())
+
+print(protein_translate)
+
+x = protein_translate.split("*")
+
+print(x)
